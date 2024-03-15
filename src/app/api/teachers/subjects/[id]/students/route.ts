cf8d239d210376aeb30cf739b2c1860e30a62add
quilantera <gugaquilante@gmail.com>
2024-03-14 22:27:22 -0300

    arrumar bug dynamic force

--- src/app/api/teachers/subjects/[id]/students/route.ts
+++ src/app/api/teachers/subjects/[id]/students/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { Context } from 'vm';
-
+export const dynamic = 'force-dynamic'
 export async function GET(request: Request, context: Context) {
   try {
     const idUser = request.headers.get('idUser');
