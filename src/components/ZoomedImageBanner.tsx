aaf255db46bd4a862bb67c77535abdefd28e023e
quilantera <gugaquilante@gmail.com>
2024-11-22 12:27:24 -0300

    Arrumando o magnifier

--- src/components/ZoomedImageBanner.tsx
+++ src/components/ZoomedImageBanner.tsx
@@ -11,13 +11,13 @@ export function ZoomedImageBanner({onClose, imageUrl}:ZoomedImageBannerProps){
     const [position, setPosition] = useState({ x: 0, y: 0 });
     const [showMagnifier, setShowMagnifier] = useState(false);
     const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-    const handleMouseMove = (e: any) => {
-        const { left, top, width, height } = e.currentTarget.getBoundingClientRect();
-        const x = ((e.pageX - left) / width) * 100;
-        const y = ((e.pageY - top) / height) * 100;
-        setPosition({ x, y });
-        setCursorPosition({ x: e.pageX - left, y: e.pageY - top });
-      };
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+      const { left, top, width, height } = e.currentTarget.getBoundingClientRect();
+      const x = ((e.clientX - left) / width) * 100; // Coordenadas relativas à largura do elemento
+      const y = ((e.clientY - top) / height) * 100;
+      setPosition({ x, y });
+      setCursorPosition({ x: e.clientX - left, y: e.clientY - top });
+    };
     return (
     <>
     <section
@@ -39,11 +39,10 @@ export function ZoomedImageBanner({onClose, imageUrl}:ZoomedImageBannerProps){
             {showMagnifier && (
               <div
                 style={{
-                  position: "absolute",
                   left: `${cursorPosition.x - 200}px`,
                   top: `${cursorPosition.y - 300}px`,
-                  pointerEvents: "none",
                 }}
+                
               >
               <div
                 className="h-[400px] w-[400px] border-slate-50 border-2 z-[50]"
