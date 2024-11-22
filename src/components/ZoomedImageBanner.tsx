389a4a1e24ad0ec60c69fffb63ed2fb3dd730aaf
quilantera <gugaquilante@gmail.com>
2024-11-22 12:17:27 -0300

    arrumando o zoom da imagem novamente

--- src/components/ZoomedImageBanner.tsx
+++ src/components/ZoomedImageBanner.tsx
@@ -20,42 +20,42 @@ export function ZoomedImageBanner({onClose, imageUrl}:ZoomedImageBannerProps){
       };
     return (
     <>
-        <section
-        style={{position:"fixed", top:0, left:0}}
-        className="w-full h-screen overflow-auto bg-gray-900 opacity-60 flex flex-col"
-       onClick={()=>onClose()}
-         >
-           
-        </section>
-        <div
-    className="fixed w-[90%] h-auto z-40 top-[24%] left-[5%]"
+    <section
+      style={{position:"fixed", top:0, left:0}}
+      className="w-full h-screen overflow-auto bg-gray-900 opacity-60 flex flex-col"
     onClick={()=>onClose()}
-    onMouseMove={handleMouseMove}
-    onMouseEnter={() => setShowMagnifier(true)}
-    onMouseLeave={() => setShowMagnifier(false)}
-  >
-    <img className="w-screen h-auto" src={imageUrl} alt="imagem ampliada" />
-
-    {showMagnifier && (
-      <div
-        style={{
-          position: "absolute",
-          left: `${cursorPosition.x - 200}px`,
-          top: `${cursorPosition.y - 300}px`,
-          pointerEvents: "none",
-        }}
       >
+        
+    </section>
         <div
-          className="h-[400px] w-[400px] border-slate-50 border-2 z-[50]"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundSize: "140vw",
-            backgroundPosition: `${position.x -4}% ${position.y -4}%`,
-          }}
-        />
-      </div>
-    )}
-  </div>
+          className="fixed w-[90%] overflow-auto h-auto z-40 top-[24%] left-[5%]"
+          onClick={()=>onClose()}
+          onMouseMove={handleMouseMove}
+          onMouseEnter={() => setShowMagnifier(true)}
+          onMouseLeave={() => setShowMagnifier(false)}
+        >
+            <img className="w-screen h-auto" src={imageUrl} alt="imagem ampliada" />
+
+            {showMagnifier && (
+              <div
+                style={{
+                  position: "absolute",
+                  left: `${cursorPosition.x - 200}px`,
+                  top: `${cursorPosition.y - 300}px`,
+                  pointerEvents: "none",
+                }}
+              >
+              <div
+                className="h-[400px] w-[400px] border-slate-50 border-2 z-[50]"
+                style={{
+                  backgroundImage: `url(${imageUrl})`,
+                  backgroundSize: "140vw",
+                  backgroundPosition: `${position.x -4}% ${position.y -4}%`,
+                }}
+              />
+            </div>
+          )}
+        </div>
     </>
     )
 }
